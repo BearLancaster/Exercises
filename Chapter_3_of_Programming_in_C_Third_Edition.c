bba09bf3e94824bcab55b3d0c4538199df42d927
BearLancaster <57752790+BearLancaster@users.noreply.github.com>
2021-07-05 23:53:49 +1000

    Chapter_3_of_Programming_in_C_Third_Edition
    This is the programming answer of the chapter 3 about Programming in C Third Edition

---
+++ Chapter_3_of_Programming_in_C_Third_Edition.c
@@ -0,0 +1,28 @@
+
+
+/* Progrmming in C Thrid Edition 
+The answer of Chapter 3(about programming)
+*/
+#include <stdio.h>
+
+
+int  main(void)
+{
+
+//Exercies  4
+int a=87;
+int b=15;
+int c=a-b;
+printf("%i-%i=%i\n",a,b,c);
+printf("%i subtracts %i is %i\n",a,b,c);
+//
+//Exercies  2
+printf("1. In C,lowercase letters are significant.\n2. main is where program execution begins.\n3. Opening and closing braces enclose  progrma statements  in a routine.\n4. All progrma statements must be terminated by a semicolon.\n");
+//
+
+//Exercies 5
+int sum;//INT sum;
+sum=25+37-19;//sum=25+37-19
+printf("The answer is %i\n" ,sum);//printf("The answer is %i\n"  sum);
+    return 0;
+}
