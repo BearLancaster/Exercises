fae71e9f3f52f561fb4a808160c72678ccf7eb15
BearLancaster <zhuangliang1822117198@gmail.com>
2023-10-31 00:35:29 +1100

    这个project是修复排序逻辑代码尝试（电视剧名称排序展示）

---
+++ MainProject/src/main/java/org/example/Main.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String map="Ep01.mp4";
+        List<String> collect=new ArrayList<>();
+        collect.add(map);
+        map="at02.mp4";
+        collect.add(map);
+        map="St03.mp4";
+        collect.add(map);
+        List<String> Temp=new ArrayList<>();
+        String []list_1=new String[collect.size()];
+        int []list_2=new int[collect.size()];
+        int i=0;
+        for(String file:collect)
+        {
+
+            String temp_file=file.replace(".MP4","").replace(".mp4","");
+            String REGEX = "[^0-9]";
+
+            String ticket = Pattern.compile(REGEX).matcher(temp_file).replaceAll("").trim();
+
+            list_1[i]=file;
+            list_2[i]=Integer.valueOf(ticket);
+            i++;
+        }
+        int temp;
+        String temp_string;
+        for (int d = 0; d < list_2.length; d++)
+        {
+            for (int j = d+1; j < list_2.length; j++) {
+                if (list_2[d] > list_2[j]) {
+
+                    temp = list_2[d];
+                    temp_string=list_1[d];
+                    list_2[d] = list_2[j];
+                    list_1[d]=list_1[j];
+                    list_2[j] = temp;
+                    list_1[j]=temp_string;
+                }
+            }
+        }
+        List<String> Sec_list=new ArrayList<>();
+        for(String fileName:list_1)
+        {
+            Sec_list.add(fileName);
+        }
+        for (String name:Sec_list)
+        {
+            System.out.println(name);
+        }
+
+
+    }
+}
